8747811b643e161e261ca29957166fbf6517ef7a
crissdot <cris-2-11-2001@hotmail.com>
2022-02-13 21:01:08 -0600

    add image onto canvas

---
+++ main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
+const imageObj = new Image();
+
+imageObj.onload = () => {
+    canvas.height = canvas.width * (imageObj.height / imageObj.width);
+    const canvas2 = document.createElement('canvas');
+    const context2 = canvas2.getContext('2d');
+
+    canvas2.width = imageObj.width / 2;
+    canvas2.height = imageObj.height / 2;
+    context2.drawImage(imageObj, 0, 0, canvas2.width, canvas2.height);
+
+    context2.drawImage(canvas2, 0, 0, canvas2.width/2, canvas2.height/2);
+
+    context.drawImage(canvas2, 0, 0, canvas2.width/2, canvas2.height/2, 0, 0, canvas.width, canvas.height);
+    
+}
+imageObj.src = './quimica.webp';
