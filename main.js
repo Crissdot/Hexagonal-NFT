246f54218327511a00b3cb8bc85b4f6382bf8b78
crissdot <cris-2-11-2001@hotmail.com>
2022-02-24 21:00:10 -0600

    add download image

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
 const imageObj = new Image();
+const downloadBtn = document.getElementById('download');
 
 imageObj.onload = () => {
     canvas.height = canvas.width * (imageObj.height / imageObj.width);
@@ -14,10 +15,21 @@ imageObj.onload = () => {
     context2.drawImage(canvas2, 0, 0, canvas2.width/2, canvas2.height/2);
 
     context.drawImage(canvas2, 0, 0, canvas2.width/2, canvas2.height/2, 0, 0, canvas.width, canvas.height);
+
+    context.rect(10, 10, 150, 100);
+    context.stroke();
 }
 
 const inputNFT = document.getElementById('nftUpload');
 inputNFT.addEventListener('change', e => {
     file = e.target.files[0];
     imageObj.src = URL.createObjectURL(file);
+    downloadBtn.style.display = 'block';
 });
+
+downloadBtn.addEventListener('click', () => {
+    let link = document.createElement('a');
+    link.download = "aaaaaaaaa.png";
+    link.href = canvas.toDataURL("image/png", 1);
+	link.click();
+})
