c0afe491d14d56d0c37ea709b361c474027afaa7
crissdot <cris-2-11-2001@hotmail.com>
2022-02-26 11:08:30 -0600

    fix image size

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const canvas = document.getElementById('myCanvas');
+canvas.width = 400;
+canvas.height = 400;
 const context = canvas.getContext('2d');
 const imageObj = new Image();
 const downloadBtn = document.getElementById('download');
 
+imageObj.src = './sampletest.jpg';
 imageObj.onload = () => {
-    canvas.height = canvas.width * (imageObj.height / imageObj.width);
     const canvas2 = document.createElement('canvas');
+    canvas2.width = imageObj.width;
+    canvas2.height = imageObj.height;
     const context2 = canvas2.getContext('2d');
 
-    canvas2.width = imageObj.width / 2;
-    canvas2.height = imageObj.height / 2;
-    context2.drawImage(imageObj, 0, 0, canvas2.width, canvas2.height);
+    context2.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height);
 
-    context2.drawImage(canvas2, 0, 0, canvas2.width/2, canvas2.height/2);
+    context.drawImage(canvas2, 0, 0, canvas2.width, canvas2.height, 0, 0, canvas.width, canvas.height);
 
-    context.drawImage(canvas2, 0, 0, canvas2.width/2, canvas2.height/2, 0, 0, canvas.width, canvas.height);
-
-    fillCorner(context, 0, 125, 0, 0, 75, 0);
-    fillCorner(context, 300, 125, 300, 0, 225, 0);
-    fillCorner(context, 0, 175, 0, 300, 75, 300);
-    fillCorner(context, 300, 175, 300, 300, 225, 300);
+    fillCorner(context, 0, imageObj.height*40/100, 0, 0, imageObj.width*25/100, 0);
+    fillCorner(context, imageObj.width, imageObj.height*40/100, imageObj.width, 0, imageObj.width*75/100, 0);
+    fillCorner(context, 0, imageObj.height*60/100, 0, imageObj.height, imageObj.width*25/100, imageObj.height);
+    fillCorner(context, imageObj.width, imageObj.height*60/100, imageObj.width, imageObj.height, imageObj.width*75/100, imageObj.height);
 }
 
 const fillCorner = (context, x1, y1, x2, y2, x3, y3) => {
@@ -39,6 +39,8 @@ const inputNFT = document.getElementById('nftUpload');
 inputNFT.addEventListener('change', e => {
     file = e.target.files[0];
     imageObj.src = URL.createObjectURL(file);
+    imageObj.height = 400;
+    imageObj.width = 400;
     downloadBtn.style.display = 'block';
 });
 
